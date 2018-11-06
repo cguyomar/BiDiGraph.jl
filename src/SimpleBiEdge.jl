4d89cbb6f98dfa072bb49fb108798b806e9e39ca
cguyomar <cguyomar@irisa.fr>
2018-11-06 11:18:36 +0100

    More functions for bidirceted edges

--- src/SimpleBiEdge.jl
+++ src/SimpleBiEdge.jl
@@ -1,5 +1,6 @@
 
 import LightGraphs: AbstractEdge, AbstractSimpleEdge
+import Base: ==
 export SimpleBiEdge
 
 struct SimpleBiEdge <: AbstractSimpleEdge{Int}
@@ -9,4 +10,13 @@ struct SimpleBiEdge <: AbstractSimpleEdge{Int}
     outdir::String
 end
 
+# Accessors
+indir(e::SimpleBiEdge) = e.indir
+outdir(e::SimpleBiEdge) = e.outdir
+
+# I/O
+show(io::IO, e::SimpleBiEdge) = print(io, "Edge $(e.src) ($(e.indir)) => $(e.dst) ($(e.outdir))")
+
 SimpleBiEdge(t::Tuple) = SimpleBiEdge(t[1], t[2],t[3],t[4])
+
+==(e1::SimpleBiEdge, e2::SimpleBiEdge) = (src(e1) == src(e2) && dst(e1) == dst(e2) && indir(e1) == indir(e2) && outdir(e1) == outdir(e2))
